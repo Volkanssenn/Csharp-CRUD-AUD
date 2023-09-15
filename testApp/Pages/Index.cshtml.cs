8ec38d9f4eab6b6ab596dc3f168657b361413579
Volkan Sen <volkanssenn@gmail.com>
2023-09-15 18:13:11 +0300

    listProducts

--- testApp/Pages/Index.cshtml.cs
+++ testApp/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace testApp.Pages
 {
@@ -11,10 +12,57 @@ namespace testApp.Pages
         {
             _logger = logger;
         }
+        public List<ProductsInfo> ListProduct = new List<ProductsInfo>();
+
 
         public void OnGet()
         {
+            try
+            {
+                string connectionString = "Data Source=KAIS;Initial Catalog=testArea;Integrated Security=True";
 
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM products";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ProductsInfo productInfo = new ProductsInfo();
+                                productInfo.id = "" + reader.GetInt32(0);
+                                productInfo.title = reader.GetString(1);
+                                productInfo.description = reader.GetString(2);
+                                productInfo.ImageUrl = reader.GetString(3);
+
+                                ListProduct.Add(productInfo);
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString);
+            }
         }
+        public void OnPost() 
+        {
+
+            
+        }
+
+        public class ProductsInfo
+        {
+            public string id;
+            public string title;
+            public string description;
+            public string ImageUrl;
+        }
+
+
     }
 }
---
+++ testApp/Pages/urun-ozellikleri.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace testApp.Pages
+{
+    public class urun_ozellikleriModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
